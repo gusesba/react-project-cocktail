85891d6302ca05edcfc11a6ae8302068c6eee1b4
Gusesba <gustavoesmanhotto@hotmail.com>
2021-09-14 18:24:40 -0300

    commit inicial

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt ad
+        recusandae culpa totam, non saepe fugiat quam possimus! Aut perferendis,
+        adipisci libero earum enim eveniet odio harum dolorem cum provident
+        deleniti aperiam a assumenda qui quas, ut velit reiciendis? Sit.
+      </p>
+    </section>
+  );
+};
+
+export default About;
